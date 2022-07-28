bb6338431534110d3ff353aa575e0d9525e0e7dd
nitin reddy <reddynitin3@gmail.com>
2022-07-29 01:01:35 +0530

    Added the circleci configurations

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App ()
+{
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>Successfully Deployed <code>A React application</code> to Netlify with CircleCI</p>
         <a
           className="App-link"
           href="https://reactjs.org"
