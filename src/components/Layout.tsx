a55fed3cc7b3b4418d3902b9ab76d9d718aa1249
nitin reddy <reddynitin3@gmail.com>
2022-07-31 18:13:37 +0530

    Refactored the components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Col, Row } from 'antd';
 import Header from './Header';
 import { Content } from 'antd/lib/layout/layout';
 
@@ -12,13 +11,8 @@ const Layout = ( props: LayoutProps ) => ( <>
   <Header />
   <Content style={ { padding: '0 50px' } }>
     <div className="site-layout-content">
-
       { props.children }
     </div>
-    {/* <Row>
-      <Col span={ 12 } offset={ 3 }>
-      </Col>
-    </Row> */}
   </Content>
 </> );
 
